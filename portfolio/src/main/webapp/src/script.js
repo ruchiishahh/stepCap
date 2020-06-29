8e85a7ea293dc7b18e318e843253ce3d6df9041b
Owen Zhang <owenzhang76@gmail.com>
2020-06-29 23:43:13 +0000

    error with implementing react-router

--- portfolio/src/main/webapp/src/script.js
+++ portfolio/src/main/webapp/src/script.js
@@ -16,18 +16,26 @@ import ReactDOM from "react-dom";
 
 import AppContainer from "./AppContainer"
 
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <AppContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// ReactDOM.render(<AppContainer />, document.getElementById("root"));
+
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+// function addRandomGreeting() {
+//   const greetings =
+//       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+//   // Pick a random greeting.
+//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+//   // Add it to the page.
+//   const greetingContainer = document.getElementById('greeting-container');
+//   greetingContainer.innerText = greeting;
+// }

cf256967dde8144ede39fe423af00c406b39a0f6
Yu Wu <chaconnewu@users.noreply.github.com>
2020-07-01 12:00:35 -0700

    Revert "Add redux code structure to The Commons"

--- portfolio/src/main/webapp/src/script.js
+++ portfolio/src/main/webapp/src/script.js
@@ -1,26 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
-import rootStore from './stores/rootStore';
-
+import './style.css';
 import AppContainer from "./AppContainer"
 
-(async (window) => {
-  const initialState = {};
-  const history = createBrowserHistory({ basename: environment.route.baseRoute });
-  const store = rootStore(initialState, history);
+ReactDOM.render(
+  <React.StrictMode>
+    <AppContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-  const rootEl = document.getElementById('root');
-  const render = (Component, el) => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Component history={history} dispatch={store.dispatch} />
-      </Provider>,
-      el
-    );
-  };
-
-  render(AppContainer, rootEl);
-})(window);

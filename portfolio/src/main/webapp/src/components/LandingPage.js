9307449c6f87d705cf8af895f80e5061c46d0de8
Owen Zhang <owenzhang76@gmail.com>
2020-07-01 14:02:45 -0700

    delete dup. landing

--- portfolio/src/main/webapp/src/components/LandingPage.js
+++ portfolio/src/main/webapp/src/components/LandingPage.js
@@ -11,12 +11,15 @@ export default class LandingPage extends Component {
 
   render() {
     return (
-      <div class="landing-main-container">
-        <Navbar />
-        <div class="landing-body-container">
-          <RegisterForm />
+        <div class="landing-main-container">
+            <div class="landing-body-container">
+                <div id="landing-title" class="center-title">THECOMMONS</div>
+                <div id="landing-tagline" class="center-text">Help anyone. Get help on anything.</div>
+                <div class="register-form-container">
+                    <RegisterForm />
+                </div>
+            </div>
         </div>
-      </div>
     );
-  }
+}
 }

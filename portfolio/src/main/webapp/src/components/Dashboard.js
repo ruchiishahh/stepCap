09dec1c20d31a4831791facfc02acfa488d9aca7
Ruchi Shah <to.ruchi08@gmail.com>
2020-07-08 10:41:05 -0700

    Add confirmed requests column & styling changes

--- portfolio/src/main/webapp/src/components/Dashboard.js
+++ portfolio/src/main/webapp/src/components/Dashboard.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import PendingReqs from "./bookingcomponents/PendingReqs";
 
 export default class BookingForm extends React.Component {
@@ -8,9 +11,26 @@ export default class BookingForm extends React.Component {
   render() {
     return (
       <main>
-        <h1>Dashboard Page</h1>
-        <h2>Here are your Pending Requests</h2>
-        <PendingReqs />
+        <h1 className="dashboard-center-title">Dashboard Page</h1>
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Paper elevation={3}>
+                <h2 className="dashboard-requests-center-title">
+                  Here are your Pending Requests
+                </h2>
+              </Paper>
+              <PendingReqs />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper>
+                <h2 className="dashboard-requests-center-title">
+                  Here are your Confirmed Requests
+                </h2>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </main>
     );
   }

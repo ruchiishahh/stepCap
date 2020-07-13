f29377b26770094138eaebb5bcb215762abee4ef
Owen Zhang <owenzhang76@gmail.com>
2020-07-13 12:40:50 -0700

    add import for font-awesome, add more design to bookService

--- portfolio/src/main/webapp/src/components/BookService.js
+++ portfolio/src/main/webapp/src/components/BookService.js
@@ -61,12 +61,40 @@ export default class BookService extends React.Component {
                             </div>
                         </div>
                         <div class="book-service-row">
-                            <div class="book-service-detail"></div>
-
+                            <div class="book-service-detail-container">
+                                <div class="book-service-detail-section">
+                                    <div id="book-service-detail-title" class="book-service-title">Overview</div>
+                                    <div class="book-service-paragraph">I will teach all the fundamental skills on a computer so that your family member can be self sufficient. Package includes walkthrough of how to download software independently, learing Microsoft Word, Powerpoint, and excel, how to create and check your email, and helpful sites to use. I will also install anti-malware software for your prefered browser.</div>
+                                </div>
+                                <div class="book-service-detail-section">
+                                    <div id="book-service-detail-title" class="book-service-title">Highlights</div>
+                                    <div class="book-service-highlight">Learn Microsoft Word, Powerpoint, Excel</div>
+                                    <div class="book-service-highlight">Learn how to setup and use an online email account</div>
+                                    <div class="book-service-highlight">Learn how to install software online</div>
+                                    <div class="book-service-highlight">Learn how detect and avoid viruses and scams</div>
+                                </div>
+                            </div>
+                            <div class="book-service-factcard">
+                                <div id="book-service-fact-title" class="book-service-title">Key Details</div>
+                                <div class="book-service-facts-container">
+                                    <div class="book-service-fact"><i class="fas fa-location-arrow"></i>I will meet you at your home</div>
+                                    <div class="book-service-fact"><i class="far fa-clock"></i>1-2 hours</div>
+                                    <div class="book-service-fact"><i class="fas fa-exclamation-circle"></i>You will need a working computer</div>
+                                    <div class="book-service-fact"><i class="fas fa-phone"></i>(124) 315-5592</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book-service-row">
+                            <div class="book-service-review-container">
+                                <div class="book-service-detail-section">
+                                    <div id="book-service-review-title" class="book-service-title">Reviews</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
         );
     }
 }

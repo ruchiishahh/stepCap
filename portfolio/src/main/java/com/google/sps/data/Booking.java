6676781379febf3dd0f6aec643db84135faa5378
Ruchi <to.ruchi08@gmail.com>
2020-07-15 01:01:13 +0000

    fixed property in Booking.java

--- portfolio/src/main/java/com/google/sps/data/Booking.java
+++ portfolio/src/main/java/com/google/sps/data/Booking.java
@@ -12,13 +12,13 @@ public final class Booking {
   private final String booking_name;
   private final String booking_date;
   private final long booking_duration;
-  private final String booking_description;
+  private final String booking_optional_note;
   private final long booking_price;
   //private boolean is_confirmed_customer;
   //private boolean is_confirmed_provider;
   private final long timestamp;
 
-  public Booking(long booking_id, String booking_name, String booking_date, long booking_duration, String booking_description, long booking_price, long timestamp) {
+  public Booking(long booking_id, String booking_name, String booking_date, long booking_duration, String booking_optional_note, long booking_price, long timestamp) {
     this.booking_id = booking_id;
     //this.customer_id = customer_id;
     //this.provider_id = provider_id;
@@ -26,7 +26,7 @@ public final class Booking {
     this.booking_name = booking_name;
     this.booking_duration = booking_duration;
     this.booking_date = booking_date;
-    this.booking_description = booking_description;
+    this.booking_optional_note = booking_optional_note;
     this.booking_price = booking_price;
     //this.is_confirmed_customer = is_confirmed_customer;
     //this.is_confirmed_provider = is_confirmed_provider;

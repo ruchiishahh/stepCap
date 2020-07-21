97389a53ef3a00b4e7dff1d85cee71ea6d3f14ad
Ruchi <to.ruchi08@gmail.com>
2020-07-21 21:48:01 +0000

    Fixed timestamp on profile & added services

--- portfolio/src/main/webapp/src/components/ServicesCreated.js
+++ portfolio/src/main/webapp/src/components/ServicesCreated.js
@@ -10,31 +10,44 @@ export default function VirtualizedList(props) {
   const { name, description, provider, rating } = props;
   return (
     <div>
-      <ListItem alignItems="flex-start">
+        <ListItem alignItems="flex-start">
+            <div class="profile-service">
+                  <div class="profile-service-image-container">
+                    <div class="profile-service-RSVP">RSVP</div>
+                  </div>
+                  <div class="profile-service-info">
+                    <div class="profile-service-info-column">
+                      <div class="profile-service-title">{name}</div>
+                      <Rating name="read-only" value={rating} readOnly />
+                      <div class="profile-service-price">$15/hr</div>
+                    </div>
+                    <div>{description}</div>
+                    
+                  </div>
+            </div>
+        </ListItem>
+      {/*<ListItem alignItems="flex-start">
         <ListItemText
-          class="profile-review"
           primary={
             <React.Fragment>
               <div class="profile-service">
-                        <div class="profile-service-image-container">
-                          <div class="profile-service-RSVP">RSVP</div>
-                        </div>
-                        <div class="profile-service-info">
-                          <div class="profile-service-info-column">
-                            <div class="profile-service-title">
-                              {name}
-                              <Rating name="read-only" value={4} readOnly />
-                              <p> {description} </p>
-                            </div>
-                            
-                          </div>
-                          <div class="profile-service-price">$24/hr</div>
-                        </div>
-              </div>
+                  <div class="profile-service-image-container">
+                    <div class="profile-service-RSVP">RSVP</div>
+                  </div>
+                  <div class="profile-service-info">
+                    <div class="profile-service-info-column">
+                      <div class="profile-service-title">{name}</div>
+                      <div class="profile-service-rating">{rating}</div>
+                      {description}
+                    </div>
+                    <div class="profile-service-price">$15/hr</div>
+                  </div>
+               </div>
             </React.Fragment>
           }
         />
-      </ListItem>
+      </ListItem> */}
+
       <Divider />
     </div>
   );

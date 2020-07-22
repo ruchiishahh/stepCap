e0fea6e5e3365662fb536b752ecd0a034e5a4b2b
Erin <erinrlimbo@gmail.com>
2020-07-22 17:27:25 +0000

    Display marker.

--- portfolio/src/main/webapp/src/components/MapContainer.jsx
+++ portfolio/src/main/webapp/src/components/MapContainer.jsx
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
+  constructor(){
+    super();
+
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
+
+    this.state = {
+        latitude: null,
+        longitude: null,
+    }
+  }
+
+
+  getCurrentLocation = () => {
+      console.log("in function");
+    if (navigator.geolocation) {
+      console.log("deeper function");
+      navigator.geolocation.watchPosition(position => {
+          this.setState({
+              longitude: position.coords.longitude,
+              latitude: position.coords.latitude,
+          })
+      });
+    } else {
+        console.log("No location found.")
+    }
+  }
+
   render() {
+      {this.getCurrentLocation()};
+      const latitude = this.state.latitude;
+      const longitude = this.state.longitude;
+
       return (
-        <Map
-          google={this.props.google}
-          zoom={8}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
+        <div className="map-container">
+            <Map
+            google={this.props.google}
+            zoom={8}
+            initialCenter={{ lat: latitude, lng: longitude}}
+            style={{height: '60%', width: '50%'}}
+            >
+                <Marker position={{ lat: latitude, lng: longitude}} />
+            </Map>
+        </div>
     );
   }
 }

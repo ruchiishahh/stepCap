1fad20e89dad9917a06c537b5652b51572ca63dc
Erin Ray Limbo <erinlimbo@berkeley.edu>
2020-07-25 22:25:20 -0700

    Kept filters but made it stay.

--- portfolio/src/main/webapp/src/components/SearchPage.jsx
+++ portfolio/src/main/webapp/src/components/SearchPage.jsx
@@ -5,16 +5,15 @@ import Results from "./Results";
 import { debounce } from "lodash";
 import axios from "axios";
 import Navbar from "./Navbar"
-import { Link } from "react-router-dom";
 
 
 class SearchPage extends Component {
   state = {
-    // filters: [
-    //     {id:0, text: "Alphabetical", active: true},
-    //     {id:1, text: "Highest-Rated", active: true},
-    //     {id:2, text: "Lowest-Price", active: true},
-    // ],
+    filters: [
+        {id:0, text: "Alphabetical", active: true},
+        {id:1, text: "Highest-Rated", active: false},
+        {id:2, text: "Lowest-Price", active: false},
+    ],
     input: "",
     filteredResults: [],
     loadedResults: true,
@@ -33,7 +32,6 @@ class SearchPage extends Component {
 
   searchOnChange = debounce((input) => {
     this.setState({ input });
-    console.log(this.state.input);
     axios
       .post("http://localhost:8080/search-handler", { input: this.state.input })
       .then((response) => {
@@ -46,19 +44,22 @@ class SearchPage extends Component {
   }, 400);
 
   filterOnClick = (id) => {
-      console.log(id);
       this.setState(prevState => ({
         filters: prevState.filters.map(
-          filter => filter.id === id ? { ...filter, active: false } : filter
+          filter => filter.id !== id ? { ...filter, active: false } : { ...filter, active: !filter.active }
         )
       }))
-      console.log(this.state.filters);
   }
 
   render() {
     return (
       <div className="searchPage-container">
         <Navbar />
+        <div>
+          {this.state.filters.map(filter => (
+            <Filter key={filter.id} filter={filter} onClick={this.filterOnClick} />
+          ))}
+        </div>
         <Search onChange={this.searchOnChange} loadedResults={this.state.loadedResults}/>
         <Results input={this.state.input} results={this.state.filteredResults}/>
       </div>
